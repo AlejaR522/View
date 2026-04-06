38042091c87cc5d9cb133d7dc2c38e38bd3696d9
Raquelxx <raquelmd.dev@gmail.com>
2026-04-05 20:47:31 -0500

    Panel de Admin funcionando

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 import { useNavigate } from "react-router-dom";
+import { getSession } from "../lib/auth";
 
 
 export default function Admin() {
@@ -11,6 +12,9 @@ export default function Admin() {
     }, []);
 
     const getUsers = async () => {
+        const session = await getSession();
+        const currentEmail = session.user.email;
+
         const { data, error } = await supabase
             .from("users")
             .select(`
@@ -20,7 +24,13 @@ export default function Admin() {
             `);
 
     if (error) console.log(error);
-        else setUsuarios(data);
+        else {
+            const filtered = data.filter(
+            user => user.correo !== currentEmail
+        );
+
+        setUsuarios(filtered);
+        }
     };
 
     const navigate = useNavigate();
@@ -30,27 +40,33 @@ export default function Admin() {
         navigate("/");
     };
 
-    return (
-        <div className="p-10">
-            <h1 className="text-2xl mb-6">Panel Admin</h1>
+return (
+    <div className="min-h-screen bg-black p-10">
+        
+        <div className="flex justify-between items-center mb-6">
+            <h1 className="text-3xl text-white font-bold">
+                Panel Admin
+            </h1>
 
-            <div className="border p-4">
-                {usuarios.map((user) => (
-                <div key={user.id} className="border-b py-2">
-                    <p>{user.nombre}</p>
-                    <p>{user.correo}</p>
-                    <p className="text-xs">
-                        {user.password_hash}
-                    </p>
-                </div>
-            ))}
-            </div>
             <button
                 onClick={handleLogout}
-                    className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800"
+                    className="bg-white text-black px-4 py-2 rounded hover:bg-gray-200"
                 >
                 Cerrar sesión
             </button>
         </div>
+
+        <div className="bg-white rounded-lg shadow-lg p-6">
+            {usuarios.map((user) => (
+                <div key={user.id} className="border-b py-3">
+                    <p className="text-black font-semibold">{user.nombre}</p>
+                    <p className="text-black">{user.correo}</p>
+                    <p className="text-xs text-gray-700 break-all">
+                        {user.password_hash}
+                    </p>
+                </div>
+            ))}
+        </div>
+    </div>
     );
 }
